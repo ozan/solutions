7b4abfafbebed965e08f665a411e10344339a907
Ozan Onay <ozan.onay@gmail.com>
2016-04-27 14:48:30 -0700

    Getting fancy

--- euler0002.hs
+++ euler0002.hs
@@ -1,6 +1,5 @@
 
-fib n
-  | n < 2 = 1
-  | otherwise = fib (n - 1) + fib (n - 2)
 
-main = print (sum (filter (even) (takeWhile (<4000000) (map fib [1..]))))
+fib = 1 : 1 : [a + b | (a, b) <- zip fib (tail fib)]
+
+main = print $ sum $ filter (even) $ takeWhile (<4000000) fib
