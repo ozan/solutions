e42499f5d8d2f61a83cddba99bca6c21abb0fa37
Ozan Onay <ozan.onay@gmail.com>
2016-04-28 22:15:39 -0700

    Add Haskell solution to anagrams

--- 4clojure-077-anagrams.hs
+++ 4clojure-077-anagrams.hs
@@ -1,3 +1,12 @@
 #!/usr/bin/env runghc
 
--- TODO
+import Data.List (sort)
+import Data.Map.Strict (insertWith, elems, empty)
+
+addWord memo word =
+  insertWith (++) (sort word) [word] memo
+
+anagrams words =
+  elems $ foldl addWord empty words
+
+main = print $ anagrams ["veer", "lake", "item", "kale", "mite", "ever"]
