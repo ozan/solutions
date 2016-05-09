2d1213b87c4875844d9c41399f9e3b8d1923b60f
Ozan Onay <ozan.onay@gmail.com>
2016-05-08 17:30:36 -0700

    Solve raindrops

--- exercism/racket/raindrops/raindrops.rkt
+++ exercism/racket/raindrops/raindrops.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
 (provide convert)
+
+(define (divides n k)
+  (= 0 (modulo n k)))
+
+(define (convert n)
+  (let ([div3 (if (divides n 3) "Pling" "")]
+        [div5 (if (divides n 5) "Plang" "")]
+        [div7 (if (divides n 7) "Plong" "")])
+    (if (string=? "" (string-append div3 div5 div7))
+        (format "~a" n)
+        (format "~a~a~a" div3 div5 div7))))
