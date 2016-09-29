220ab79ae82c9c84b3cf70f9a1cc5bd715122555
Ozan Onay <ozan.onay@gmail.com>
2016-09-29 15:51:28 -0700

    Improve style

--- exercism/c/rna-transcription/src/rna_transcription.c
+++ exercism/c/rna-transcription/src/rna_transcription.c
@@ -4,27 +4,27 @@
 
 char *to_rna(const char *dna) {
   int i, len = strlen(dna);
-  char *rna = (char *)malloc(len);
+  char *rna = (char *)malloc(len + 1);
 
   for (i = 0; i < len; i++) {
-    switch (*(dna + i)) {
+    switch (dna[i]) {
     case 'G':
-      *(rna + i) = 'C';
+      rna[i] = 'C';
       break;
     case 'C':
-      *(rna + i) = 'G';
+      rna[i] = 'G';
       break;
     case 'T':
-      *(rna + i) = 'A';
+      rna[i] = 'A';
       break;
     case 'A':
-      *(rna + i) = 'U';
+      rna[i] = 'U';
       break;
     default:
       return NULL;
     }
   }
 
-  *(rna + i) = '\0';
+  rna[i] = '\0';
   return rna;
 }
