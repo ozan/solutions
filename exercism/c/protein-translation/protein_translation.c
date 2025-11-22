174be85aaaf8c252c4e37bbc50ab715630fbf4c7
Oz Nova <ozan.onay@gmail.com>
2025-11-22 11:34:47 +1100

    Stash

--- exercism/c/protein-translation/protein_translation.c
+++ exercism/c/protein-translation/protein_translation.c
@@ -1,72 +1,61 @@
 #include "protein_translation.h"
+#include <stdint.h>
 
 #define AA_INVALID -1
 #define AA_STOP 0
 
-/* Translate the first 3 chars of given string to an amino acid, or AA_STOP or
- * AA_INVALID Uses switch statements to avoid strcmp */
+// Static lookup table for direct codon->amino_acid mapping.
+// Fully initialized to ensure non-coding codons are AA_INVALID.
+static const int amino_acid_table[64] = {
+    // Value: -1 = AA_INVALID, 0 = AA_STOP, >0 = amino_acid_t
+    /* AAA */ AA_INVALID, /* AAC */ AA_INVALID, /* AAG */ AA_INVALID, /* AAU */ AA_INVALID,
+    /* ACA */ AA_INVALID, /* ACC */ AA_INVALID, /* ACG */ AA_INVALID, /* ACU */ AA_INVALID,
+    /* AGA */ AA_INVALID, /* AGC */ AA_INVALID, /* AGG */ AA_INVALID, /* AGU */ AA_INVALID,
+    /* AUA */ AA_INVALID, /* AUC */ AA_INVALID, /* AUG */ Methionine, /* AUU */ AA_INVALID,
+    /* CAA */ AA_INVALID, /* CAC */ AA_INVALID, /* CAG */ AA_INVALID, /* CAU */ AA_INVALID,
+    /* CCA */ AA_INVALID, /* CCC */ AA_INVALID, /* CCG */ AA_INVALID, /* CCU */ AA_INVALID,
+    /* CGA */ AA_INVALID, /* CGC */ AA_INVALID, /* CGG */ AA_INVALID, /* CGU */ AA_INVALID,
+    /* CUA */ AA_INVALID, /* CUC */ AA_INVALID, /* CUG */ AA_INVALID, /* CUU */ AA_INVALID,
+    /* GAA */ AA_INVALID, /* GAC */ AA_INVALID, /* GAG */ AA_INVALID, /* GAU */ AA_INVALID,
+    /* GCA */ AA_INVALID, /* GCC */ AA_INVALID, /* GCG */ AA_INVALID, /* GCU */ AA_INVALID,
+    /* GGA */ AA_INVALID, /* GGC */ AA_INVALID, /* GGG */ AA_INVALID, /* GGU */ AA_INVALID,
+    /* GUA */ AA_INVALID, /* GUC */ AA_INVALID, /* GUG */ AA_INVALID, /* GUU */ AA_INVALID,
+    /* UAA */ AA_STOP,    /* UAC */ Tyrosine,   /* UAG */ AA_STOP,    /* UAU */ Tyrosine,
+    /* UCA */ Serine,     /* UCC */ Serine,     /* UCG */ Serine,     /* UCU */ Serine,
+    /* UGA */ AA_STOP,    /* UGC */ Cysteine,   /* UGG */ Tryptophan, /* UGU */ Cysteine,
+    /* UUA */ Leucine,    /* UUC */ Phenylalanine, /* UUG */ Leucine, /* UUU */ Phenylalanine,
+};
 
+
+// Maps a single character to a 2-bit value at runtime.
+static int c2b(char c) {
+  switch (c) {
+  case 'A':
+    return 0;
+  case 'C':
+    return 1;
+  case 'G':
+    return 2;
+  case 'U':
+    return 3;
+  default:
+    return 64; // guarantee handled as error
+  }
+}
+
+/* Translate the first 3 chars of given string to an amino acid, or AA_STOP or
+ * AA_INVALID. Now uses a direct, fully-initialized lookup table. */
 int trans(const char *s) {
   if (s == NULL)
     return AA_INVALID;
 
-  char a = *s, b = *(s + 1), c = *(s + 2);
+  // Compute lookup index
+  int idx = (c2b(s[0]) << 4) + (c2b(s[1]) << 2) + c2b(s[2]);
 
-  if (a == 'A' && b == 'U' && c == 'G')
-    return Methionine;
+  if (idx < 0 || idx > 63)
+    return AA_INVALID;
 
-  if (a != 'U')
-    return AA_INVALID; // other than methionine all start U
-
-  switch (b) {
-  case 'A':
-    switch (c) {
-    case 'U':
-    case 'C':
-      return Tyrosine;
-    case 'A':
-    case 'G':
-      return AA_STOP;
-    default:
-      return AA_INVALID;
-    }
-  case 'U':
-    switch (c) {
-    case 'A':
-    case 'G':
-      return Leucine;
-    case 'U':
-    case 'C':
-      return Phenylalanine;
-    default:
-      return AA_INVALID;
-    }
-  case 'C':
-    switch (c) {
-    case 'A':
-    case 'U':
-    case 'C':
-    case 'G':
-      return Serine;
-    default:
-      return AA_INVALID;
-    }
-  case 'G':
-    switch (c) {
-    case 'U':
-    case 'C':
-      return Cysteine;
-    case 'G':
-      return Tryptophan;
-    case 'A':
-      return AA_STOP;
-    default:
-      return AA_INVALID;
-    }
-  }
-
-  // Default case for any other input (like the original return -1)
-  return AA_INVALID; // Assuming UNKNOWN is defined as -1 or similar
+  return amino_acid_table[idx];
 }
 
 protein_t protein(const char *const rna) {
