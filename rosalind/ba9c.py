72ea82114e7d74564d7e822b482569335053210b
Oz Nova <ozan.onay@gmail.com>
2025-12-01 12:32:07 +1100

    Stash

--- rosalind/ba9c.py
+++ rosalind/ba9c.py
@@ -10,65 +10,113 @@ def leaves(g, node):
             if k == '$':
                 out.append(v)
             else:
-                stack.append(v)
+                stack.append(v[0])
     return out
 
 
-def ba9c(string):
+def modified_suffix_trie(text):
+    """
+    Construct a modified suffix trie, per the pseudocode on p 165
+    """
     g = {0: {}}
 
     # build the trie
-    for start in range(len(string)):
+    for start in range(len(text)):
         i = start
         node = 0
-        while i < len(string):
-            c = string[i]
+        while i < len(text):
+            c = text[i]
+            # each value was previously the node id
+            # now it should be (node_id, first_i, len)
             if c in g[node]:
-                node = g[node][c]
+                node = g[node][c][0]
             else:
                 if c == '$':
                     g[node][c] = start
                 else:
                     nxt = len(g)
-                    g[node][c] = nxt
+                    g[node][c] = (nxt, i, 1)
                     g[nxt] = {}
                     node = nxt
             i += 1
+    return g
 
-    for case in ('antenna', 'ana', 'as'):
-        node = 0
-        for c in case:
-            if c in g[node]:
-                node = g[node][c]
-            else:
-                print(f'{case} not found')
-                break
+
+def sufix_tree(g):
+    """
+    Convert the modified suffix trie to a tree by replacing each non-branching path depth first
+    """
+    stack = [0]
+    while stack:
+        n = stack.pop()
+        for k, v in g[n].items():
+            if k == '$': continue
+            nxt_node, first_i, lngth = v
+            while len(g[nxt_node]) == 1:
+
+                kk = list(g[nxt_node].keys())[0]
+                try:
+                    nxt_nxt_node = g[nxt_node][kk][0]
+                    del g[nxt_node]
+                    nxt_node = nxt_nxt_node
+                    lngth += 1
+                except TypeError:  # could be end of string TODO cleanup
+                    lngth += 1
+                    break
+            g[n][k] = (nxt_node, first_i, lngth)
+            stack.append(nxt_node)
+    return g
+
+
+def find(tree, pattern):
+    """
+    Find the given pattern in the suffix tree, if any
+
+    Not actually required for the problem, but good to know that we can!
+    """
+    node = 0
+    i = 0
+    while i < len(pattern):
+        c = pattern[i]
+        if c in tree[node]:
+            match = tree[node][c]
+            node = match[0]
+            i += match[2]
         else:
-            print(f'{case} FOUND starting at {leaves(g, node)}')
-
-    return
-
-    # TODO check text for better way compact into a tree
-    for k, v in list(g.items()):
-        for s, nxt in list(v.items()):
-            while nxt in g and len(g[nxt]) == 1:
-                s2, nnxt = list(g[nxt].items())[0]
-                # print(f'{s} -> {s+s2}, {nxt} -> {nnxt}')
-                g[k][s+s2] = nnxt
-                del g[k][s]
-                del g[nxt]
-                nxt, s = nnxt, s + s2
-
-    # print(g)
-    for k, v in g.items():
-        for s in v.keys():
-            print(s)
-    # print(string)
+            return []
+    return leaves(tree, node)
 
 
-# ba9c('hi$')
-ba9c('panamabananas$')
-# ba9d('ATATCGTTTTATCGTT$')
+def print_edges(tree, text):
+    stack = [0]
+    while stack:
+        node = stack.pop()
+        for k, v in tree[node].items():
+            if k == '$':
+                continue
+            print(text[v[1]:v[1]+v[2]])
+            stack.append(v[0])
+
+
+def ba9c(text):
+    g = modified_suffix_trie(text)
+
+    gg = sufix_tree(g)
+
+    """
+    for pattern in ('antenna', 'ana', 'as'):
+        idxs = find(gg, pattern)
+        if idxs:
+            print(f'{pattern} FOUND starting at {", ".join(str(n) for n in idxs)}')
+        else:
+            print(f'{pattern} not found')
+    """
+
+    print_edges(gg, text)
+
+
+# ba9c('panamabananas$')
+ba9c('ATAAATG$')
 
 # with open('/Users/oz/Downloads/rosalind_ba9c.txt') as f: ba9d(f.read().rstrip())
 
--- rosalind/ba9d.py
+++ rosalind/ba9d.py
@@ -1,40 +1,130 @@
+from pprint import pprint
 
-def ba9d(string):
+
+def leaves(g, node):
+    """
+    DFS to find all leaves reachable from the given node
+    """
+    out = []
+    stack = [node]
+    while stack:
+        n = stack.pop()
+        for k, v in g[n].items():
+            if k == '$':
+                out.append(v)
+            else:
+                stack.append(v[0])
+    return out
+
+
+def modified_suffix_trie(text):
+    """
+    Construct a modified suffix trie, per the pseudocode on p 165
+    """
     g = {0: {}}
 
     # build the trie
-    for i in range(len(string)):
-        p = string[i:]  # TODO use index instead of slice
+    for start in range(len(text)):
+        i = start
         node = 0
-        for c in p:
+        while i < len(text):
+            c = text[i]
+            # each value was previously the node id
+            # now it should be (node_id, first_i, len)
             if c in g[node]:
-                node = g[node][c]
+                node = g[node][c][0]
             else:
-                nxt = len(g)
-                g[node][c] = nxt
-                g[nxt] = {}
-                node = nxt
-
-    # compact into a tree
-    for k, v in list(g.items()):
-        for s, nxt in list(v.items()):
-            while nxt in g and len(g[nxt]) == 1:
-                s2, nnxt = list(g[nxt].items())[0]
-                # print(f'{s} -> {s+s2}, {nxt} -> {nnxt}')
-                g[k][s+s2] = nnxt
-                del g[k][s]
-                del g[nxt]
-                nxt, s = nnxt, s + s2
-
-    print(g)
-    for k, v in g.items():
-        for s in v.keys():
-            print(s)
-    # print(string)
+                if c == '$':
+                    g[node][c] = start
+                else:
+                    nxt = len(g)
+                    g[node][c] = (nxt, i, 1)
+                    g[nxt] = {}
+                    node = nxt
+            i += 1
+    return g
 
 
-ba9d('ATATCGTTTTATCGTT$')
+def sufix_tree(g):
+    """
+    Convert the modified suffix trie to a tree by replacing each non-branching path depth first
+    """
+    stack = [0]
+    while stack:
+        n = stack.pop()
+        for k, v in g[n].items():
+            if k == '$': continue
+            nxt_node, first_i, lngth = v
+            while len(g[nxt_node]) == 1:
 
-# with open('/Users/oz/Downloads/rosalind_ba9c.txt') as f: ba9d(f.read().rstrip())
+                kk = list(g[nxt_node].keys())[0]
+                try:
+                    nxt_nxt_node = g[nxt_node][kk][0]
+                    del g[nxt_node]
+                    nxt_node = nxt_nxt_node
+                    lngth += 1
+                except TypeError:  # could be end of string TODO cleanup
+                    lngth += 1
+                    break
+            g[n][k] = (nxt_node, first_i, lngth)
+            stack.append(nxt_node)
+    return g
+
+
+def find(tree, pattern):
+    """
+    Find the given pattern in the suffix tree, if any
+
+    Not actually required for the problem, but good to know that we can!
+    """
+    node = 0
+    i = 0
+    while i < len(pattern):
+        c = pattern[i]
+        if c in tree[node]:
+            match = tree[node][c]
+            node = match[0]
+            i += match[2]
+        else:
+            return []
+    return leaves(tree, node)
+
+
+def print_edges(tree, text):
+    stack = [0]
+    while stack:
+        node = stack.pop()
+        for k, v in tree[node].items():
+            if k == '$':
+                continue
+            print(text[v[1]:v[1]+v[2]])
+            stack.append(v[0])
+
+
+def ba9d(text):
+    g = modified_suffix_trie(text)
+
+    pprint(g)
+
+    gg = sufix_tree(g)
+    pprint(gg)
+
+    """
+    for pattern in ('antenna', 'ana', 'as'):
+        idxs = find(gg, pattern)
+        if idxs:
+            print(f'{pattern} FOUND starting at {", ".join(str(n) for n in idxs)}')
+        else:
+            print(f'{pattern} not found')
+    """
+
+    print_edges(gg, text)
+
+
+ba9d('banana')
+
+# ba9d('ATATCGTTTTATCGTT$')
+
+# with open('/Users/oz/Downloads/rosalind_ba9d.txt') as f: ba9d(f.read().rstrip())
 
 
